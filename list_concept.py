9691ab2ad6f06f1f8fb8a69ac30675c61add7c34
karma <ad355168@gmail.com>
2025-06-12 12:25:12 +0530

    adding the concepts i learnt today

---
+++ list_concept.py
@@ -0,0 +1,3 @@
+x=[12,24,36];
+print(x);
+print('x is an ',type(x),' variable');
---
+++ variable_concept.py
@@ -0,0 +1,11 @@
+x=100;
+print('the value of x is:', x);
+print('x is an ',type(x),' variable');
+
+y=3.14;
+print('the value of y is:', y);
+print('y is an ',type(y),' variable');
+
+z='aditya';
+print('the value of z is: ',z);
+print('z is an ',type(z),' variable');
