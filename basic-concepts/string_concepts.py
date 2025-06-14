cee28ee1b8f19a97fa25d495f7eefd9231042f53
karma <ad355168@gmail.com>
2025-06-14 22:09:54 +0530

    added string concept

---
+++ basic-concepts/string_concepts.py
@@ -0,0 +1,12 @@
+var = "cutiepie";
+
+print("the first letter of the string is :");
+print(var[0]);
+print("the first letter of the string is :");
+print(var[4]);
+print("this is an extracted segment from the beginning of the string :");
+print(var[0:3]);
+print("this is an extracted segment from the some point of the string till the end");
+print(var[3:]);
+print("the length of the string is: ");
+print(len(var));
