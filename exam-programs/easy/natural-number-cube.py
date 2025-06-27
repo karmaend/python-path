e1fc07b3f82c90303f3180697d0fb17303755661
karma <ad355168@gmail.com>
2025-06-27 12:51:03 +0530

    optimized squares and cubes

--- exam-programs/easy/natural-number-cube.py
+++ exam-programs/easy/natural-number-cube.py
@@ -1,6 +1,7 @@
+import math
 x=int(input("enter the limit: "))
 sum=0
 for i in range(0,x+1):
-    sum+=i*i*i
+    sum+=i**3
     
 print(f"sum of natural numbers till {x} is: ", sum)
--- exam-programs/easy/natural-number-square.py
+++ exam-programs/easy/natural-number-square.py
@@ -1,6 +1,7 @@
+import math
 x=int(input("enter the limit: "))
 sum=0
 for i in range(0,x+1):
-    sum+=i*i
+    sum+=i**2
     
 print(f"sum of natural numbers till {x} is: ", sum)
