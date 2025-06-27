faca3687ed61c5152f284e4219f84eba28babe60
karma <ad355168@gmail.com>
2025-06-27 12:47:37 +0530

    added some square and cube programs

---
+++ exam-programs/easy/ascii-value.py
@@ -0,0 +1,4 @@
+print("enter the character you want the ascii value of: ")
+x=input()
+
+print("the ascii value of '"+ x + "' is: ", ord(x))
---
+++ exam-programs/easy/natural-number-square.py
@@ -0,0 +1,6 @@
+x=int(input("enter the limit: "))
+sum=0
+for i in range(0,x+1):
+    sum+=i*i
+    
+print(f"sum of natural numbers till {x} is: ", sum)
